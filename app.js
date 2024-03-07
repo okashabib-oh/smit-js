ac8bfae87cdbed8d33140361933d6a2978f1fdbd
Okasha Habib <160734391+okashabib-oh@users.noreply.github.com>
2024-03-07 20:09:35 +0500

    Calculation Task

--- app.js
+++ app.js
@@ -6,3 +6,39 @@ function addNumber() {
   alert(`The sum of ${firstNumber} and ${secondNumber} is ${answerAdd}.`);
   console.log(`The sum of ${firstNumber} and ${secondNumber} is ${answerAdd}.`);
 }
+function subtractNumber() {
+  let firstNumber = prompt("Enter first number: ");
+  let secondNumber = prompt("Enter second number: ");
+
+  let answerSubtract = Number(firstNumber) - Number(secondNumber);
+  alert(
+    `The subtraction of ${firstNumber} and ${secondNumber} is ${answerSubtract}.`
+  );
+  console.log(
+    `The sum of ${firstNumber} and ${secondNumber} is ${answerSubtract}.`
+  );
+}
+function multiplyNumber() {
+  let firstNumber = prompt("Enter first number: ");
+  let secondNumber = prompt("Enter second number: ");
+
+  let answerMultiply = Number(firstNumber) * Number(secondNumber);
+  alert(
+    `The multiplication of ${firstNumber} and ${secondNumber} is ${answerMultiply}.`
+  );
+  console.log(
+    `The sum of ${firstNumber} and ${secondNumber} is ${answerMultiply}.`
+  );
+}
+function divideNumber() {
+  let firstNumber = prompt("Enter first number: ");
+  let secondNumber = prompt("Enter second number: ");
+
+  let answerDivide = Number(firstNumber) / Number(secondNumber);
+  alert(
+    `The division of ${firstNumber} and ${secondNumber} is ${answerDivide}.`
+  );
+  console.log(
+    `The sum of ${firstNumber} and ${secondNumber} is ${answerDivide}.`
+  );
+}
